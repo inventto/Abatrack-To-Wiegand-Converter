f36807835c2add77be54dd45e906d561231542ef
Jose Luiz Tafarel <jltafarel@gmail.com>
2014-11-26 17:05:47 -0200

    verifying the zeros before the second part of the wiegand

--- src/invent/to/Converter.java
+++ src/invent/to/Converter.java
@@ -9,10 +9,25 @@ public class Converter {
 		String abatrackLast4 = abatrackHexa.substring(abaLength - 4);
 		String abatrackRest = abatrackHexa.substring(abaLength - 6, abaLength - 4);
 
+		int numberOfZeros = 0;
+
+		for (int i = 0; i < abatrackLast4.length(); i++) {
+			if (abatrackLast4.charAt(i) == '0') {
+				numberOfZeros++;
+			} else {
+				break;
+			}
+		}
+		System.out.println(numberOfZeros);
+
 		Long wiegandBegin = Long.parseLong(abatrackRest, 16);
+		String wiegandEndZeros = "";
+		for (int i = 0; i < numberOfZeros; i++) {
+			wiegandEndZeros += "0";
+		}
 		Long wiegandEnd = Long.parseLong(abatrackLast4, 16);
-		
-		String wiegandString = wiegandBegin.toString() + wiegandEnd.toString();
+
+		String wiegandString = wiegandBegin.toString() + wiegandEndZeros + wiegandEnd.toString();
 
 		return Long.parseLong(wiegandString);
 	}
